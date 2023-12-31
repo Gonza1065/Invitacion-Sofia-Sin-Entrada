c02286414e955aaab96a067552f2d6e2c54a053b
Gonza1065 <104340268+Gonza1065@users.noreply.github.com>
2023-12-31 15:03:23 -0300

    Agregando fondo

--- src/components/Assist.jsx
+++ src/components/Assist.jsx
@@ -10,7 +10,9 @@ export function Assist() {
         <h2>¿Podría confirmar su asistencia a través de este formulario?</h2>
       </div>
       <div className="btn-confirm-assist">
-        <Link>Confirmar Asistencia</Link>
+        <Link to="https://docs.google.com/forms/d/e/1FAIpQLSeJCN5_0tiWuVWGPnNrpkGbYkUipHkEIReDkLJvlHhrnYQuaQ/viewform?usp=sf_link">
+          Confirmar Asistencia
+        </Link>
       </div>
     </>
   );
